b8a3c4f17e93ba1476532456cd04003d8a7a343a
Maxim Prosvirkin <Nort359@gmail.com>
2018-06-26 16:09:23 +0300

    Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,41 @@
-const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+    src: path.resolve(__dirname, 'src'),
+    build: path.resolve(__dirname, 'dist'),
+};
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
+    context: PATHS.src,
     entry: './index',
 
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: PATHS.build
     },
 
-    resove: {
+    resolve: {
         extensions: ['.js']
     },
 
     plugins: [
-        new webpack.optimize.UglifyJsPlugin()
+        new UglifyJsPlugin(),
+        new HTMLWebpackPlugin({
+            title: "Личный Сайт Web-разработчика | Просвиркин Максим",
+            /*
+            minify: {
+                html5: true
+            },
+            template: './template.html'
+            */
+        })
     ],
 
     
 
-    devtools: 'eval-souse-map',
+    devtool: 'eval-souse-map',
 
-    watch: true
+    watch: false
 };
