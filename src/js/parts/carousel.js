d8244968a95e380f7ec812362cc068c6e8ed4227
Maxim Prosvirkin <Nort359@gmail.com>
2018-07-06 18:52:50 +0300

    Refactored carousel code

--- src/js/parts/carousel.js
+++ src/js/parts/carousel.js
@@ -1,12 +1,20 @@
 /* Карусель */
 
-const [fontColor, mainColor] = ["#232323", "#4EC2F8"];
+const [fontColor, mainColor] = ["#232323", "#4EC2F8"]; // Цвета для индикаторов
+
+const [screenClass, descriptionClass, indicatorClass] = [
+    "works__screens-active",
+    "works__description-active",
+    "works__indicators_dot-active"
+]; // Классы, для анимации карусели (активные элементы)
 
 const [worksScreens, worksDescription, indicators] = [
     document.getElementsByClassName("works__screens"),
     document.getElementsByClassName("works__description"),
     document.getElementsByClassName("works__indicators_dot")
-];
+]; // Массив элементов, которые необходимо анимировать каруселью
+
+const carouselTick = 10000; // Время на одну итерацию карусели
 
 /* Задаём начальный state */
 for (let i = 0; i < worksScreens.length; i++) {
@@ -18,6 +26,7 @@ for (let i = 0; i < worksScreens.length; i++) {
     worksDescription[i].style.animation = "none";
 }
 
+/* Вешаем обработчики событий на индикаторы */
 for (let i = 0; i < indicators.length; i++) {
     indicators[i].addEventListener("click", function() {
         let id = this.getAttribute("data-id");
@@ -28,14 +37,22 @@ for (let i = 0; i < indicators.length; i++) {
         clearInterval(indicatorsCarousel);
 
         /* Анимируем необходимый элемент */
-        initializeCarousel(worksScreens, id, "active_screen")();
-        initializeCarousel(worksDescription, id, "active_description")();
-        initializeCarousel(indicators, id, "works__indicators_dot-active")();
+        initializeCarousel(worksScreens, id, screenClass)();
+        initializeCarousel(worksDescription, id, descriptionClass)();
+        initializeCarousel(indicators, id, indicatorClass)();
 
         /* Снова запускаем интервалы */
-        screenCarousel = setInterval(initializeCarousel(worksScreens, +id + 1, "active_screen"), carouselTick);
-        descriptionCarousel = setInterval(initializeCarousel(worksDescription, +id + 1, "active_description"), carouselTick);
-        indicatorsCarousel = setInterval(initializeCarousel(indicators, +id + 1, "works__indicators_dot-active"), carouselTick);
+        screenCarousel = setInterval(
+            initializeCarousel(worksScreens, +id + 1, screenClass),
+        carouselTick);
+        
+        descriptionCarousel = setInterval(
+            initializeCarousel(worksDescription, +id + 1, descriptionClass),
+        carouselTick);
+
+        indicatorsCarousel = setInterval(
+            initializeCarousel(indicators, +id + 1, indicatorClass),
+        carouselTick);
     });
 }
 
@@ -64,9 +81,15 @@ function initializeCarousel(elements, current = 1, className) {
     };
 }
 
-const carouselTick = 3000;
-
 /* Запускаем карусель */
-let screenCarousel = setInterval(initializeCarousel(worksScreens, 1, "active_screen"), carouselTick);
-let descriptionCarousel = setInterval(initializeCarousel(worksDescription, 1, "active_description"), carouselTick);
-let indicatorsCarousel = setInterval(initializeCarousel(indicators, 1, "works__indicators_dot-active"), carouselTick);
+let screenCarousel = setInterval(
+    initializeCarousel(worksScreens, 1, screenClass),
+carouselTick);
+
+let descriptionCarousel = setInterval(
+    initializeCarousel(worksDescription, 1, descriptionClass),
+carouselTick);
+
+let indicatorsCarousel = setInterval(
+    initializeCarousel(indicators, 1, indicatorClass),
+carouselTick);
